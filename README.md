9d767ab07e785de102a66030f2fdac98e0489533
Rodrigo Fonseca Rocha Soares <fonsecarsrodrigo@gmail.com>
2025-12-13 12:08:56 -0300

    Adding OPENAPI integration to customer_area.py

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ pip install -r requirements.txt
 ## 🔥 3. Run the Flask Application
 
 ./run_server.sh
+
+
+##   4. Accessing the server
+
+ Open on a browser: http://127.0.0.1:5000
--- customer_area.py
+++ customer_area.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask import redirect
+from flask_openapi3 import OpenAPI, Info
 
-app = Flask(__name__)
+
+info = Info(title="Bora Orneles Customer Area API", version="1.0.0")
+app = OpenAPI(__name__, info=info)
 
 @app.route("/")
 def home():
-    return "Bora Orneles Customer Area Server is running!"
+     return redirect('/openapi')
 
 if __name__ == "__main__":
     app.run(debug=True)
